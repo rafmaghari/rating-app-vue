a61545812fd9411787b30ae58f966a169d53a2e7
rafmaghari <29rafaelmaghari@gmail.com>
2021-05-29 02:44:19 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { Vue, Component } from 'vue-property-decorator';
+import Header from '@/components/common/Header'
+import './assets/tailwind.css';
 
-#nav {
-  padding: 30px;
-}
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class App extends Vue {
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
